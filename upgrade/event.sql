c380800a40b295961a5f06bebac2e6cfbcc2a6a0
felix <felixprawira8@gmail.com>
2016-02-13 17:13:44 +0700

    'test'

---
+++ upgrade/event.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[event]
+(
+	[event_id] INT NOT NULL PRIMARY KEY, 
+	[event_eo] INT NOT NULL, 
+    [event_name] VARCHAR(50) NOT NULL, 
+    [event_desc] TEXT NOT NULL, 
+    [event_address] VARCHAR(MAX) NOT NULL, 
+    [event_startdate] DATE NOT NULL, 
+    [event_enddate] DATE NOT NULL, 
+    [event_logo] IMAGE NOT NULL 
+)
