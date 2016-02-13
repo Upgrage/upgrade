0c0068ae7019f0fedf3aea64380a4a73b28ef82f
felix <felixprawira8@gmail.com>
2016-02-13 18:10:34 +0700

    'baru'

---
+++ upgrade/eh_comp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[eh_comp]
+(
+	[eh_comp_id] INT NOT NULL PRIMARY KEY, 
+    [eh_comp_eh] INT NOT NULL, 
+    [eh_comp_competition] INT NOT NULL, 
+    [eh_comp_requirement] VARCHAR(50) NOT NULL, 
+    [eh_comp_status] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [fk_comp_eh] FOREIGN KEY ([eh_comp_eh]) REFERENCES [eh]([eh_id]),
+	CONSTRAINT [fk_comp_competition] FOREIGN KEY ([eh_comp_competition]) REFERENCES [competition]([competition_id])
+)
--- upgrade/eo.sql
+++ upgrade/eo.sql
@@ -5,6 +5,7 @@
     [eo_desc] TEXT NULL, 
     [eo_address] VARCHAR(MAX) NOT NULL, 
     [eo_contact] VARCHAR(50) NOT NULL, 
+	[eo_username] VARCHAR(50) NOT NULL, 
     [eo_email] VARCHAR(50) NOT NULL, 
     [eo_password] VARCHAR(MAX) NOT NULL, 
     [eo_referal] INT NULL, 
